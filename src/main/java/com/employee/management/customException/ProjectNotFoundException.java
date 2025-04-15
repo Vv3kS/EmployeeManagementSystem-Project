fe06eaaa11d94ceec2be7749c3d31c695214b836
Vvek <41186164+Vv3kS@users.noreply.github.com>
2025-04-15 21:55:09 +0530

    Delete src directory

--- src/main/java/com/employee/management/customException/ProjectNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.employee.management.customException;
-
-public class ProjectNotFoundException extends RuntimeException {
-    public ProjectNotFoundException (String resouce , String field , Object value){
-        super(String.format("%s not found with %s : '%s'" , resouce , field , value));
-
-    }
-}
--- src/main/java/com/employee/management/repositories/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.employee.management.repositories;
-
-import com.employee.management.entities.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
